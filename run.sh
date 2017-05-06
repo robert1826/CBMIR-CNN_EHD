46a3acf026c72f18c52cc2fe68fa87d69fa37d82
robert <robert1826@gmail.com>
2017-05-07 00:59:02 +0200

    merged the 2 scripts together

--- run.sh
+++ run.sh
@@ -8,18 +8,16 @@ echo 'done'
 echo ''
 
 echo 'Generating dataset lists'
-cd ..
-find -L . -path "*IRMA*train*png" > ./src/tmp_train.txt
-find -L . -path "*IRMA*test*png" > ./src/tmp_test.txt
-cd ./src/
+find -L .. -path "*IRMA*train*png" > ./tmp_train.txt
+find -L .. -path "*IRMA*test*png" > ./tmp_test.txt
 head tmp_train.txt -n 300 > train_dataset.txt
 head tmp_test.txt -n 20 > test_dataset.txt
 echo 'done'
 echo ''
 
 echo 'Generating training descriptors file'
-optirun python myalexnet_forward.py ../ train_dataset.txt train_codes_02.csv_labels.pickle
-optirun python myalexnet_forward.py ../ test_dataset.txt test_codes_03.csv_labels.pickle
+optirun python myalexnet_forward.py ./ train_dataset.txt train_codes_02.csv_labels.pickle
+optirun python myalexnet_forward.py ./ test_dataset.txt test_codes_03.csv_labels.pickle
 echo 'done'
 echo ''
 
@@ -28,6 +26,36 @@ python search_euclidean.py train_dataset.txt_desc test_dataset.txt_desc ../IRMA/
 echo 'done'
 echo ''
 
+# EHD part
+cd ../mpeg7fexlin/
+
+echo 'Getting dataset lists'
+cp ../src/*dataset* .
+echo 'done'
+echo ''
+
+echo 'Converting images to JPG'
+mkdir convert_dir
+for img in $(cat train_dataset.txt test_dataset.txt); do 
+    filename=$(basename $img .png)
+    convert $img ./convert_dir/$filename.jpg
+done
+echo 'done'
+echo ''
+
+echo 'Generated new imageList file for the converted images'
+find . -path "*jpg" > ./dataset-converted.txt
+echo 'done'
+echo ''
+
+echo 'Generating image descriptors'
+export LD_LIBRARY_PATH=$(pwd)/solibs/
+./MPEG7Fex EHD dataset-converted.txt ehd_out.txt
+echo 'done'
+echo ''
+
+
+cd ../src/
 rm tmp*.txt
 mkdir ../generated_files
 mv train* ../generated_files -f
@@ -35,3 +63,8 @@ mv test* ../generated_files -f
 mv Retrieval ../generated_files -f
 mv retrieval_result_fc7 ../generated_files -f
 
+# clean mpeg dir
+cd ../mpeg7fexlin/
+rm *dataset*
+rm -r convert_dir
+mv ehd_out.txt ../generated_files
