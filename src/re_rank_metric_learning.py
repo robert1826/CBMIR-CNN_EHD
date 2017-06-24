b385dd6fc6728f8a2dde42a91f1b4f2aaef0d777
robert <robert1826@gmail.com>
2017-06-24 23:29:54 +0200

    added metric_learning based on EHD

--- src/re_rank_metric_learning.py
+++ src/re_rank_metric_learning.py
@@ -25,42 +25,56 @@ def load_descriptor(name):
 def distance(a, b, M_inv):
 	return dis.mahalanobis(a, b, M_inv)
 
+def get_basename(s):
+	 num = re.search('(\d+)(?=(\.png|\.jpg))', s).group(1)
+	 return num
+
+def get_file_name(s):
+	num = re.search('(\d+)(?=(\.png|\.jpg))', s).group(1)
+	return num + '.png'
+
+def read_EHD():
+	ehd = {}
+	with open('../data_files/ehd_out.txt', 'r') as f:
+		for curLine in f.readlines():
+			filename = curLine.strip().split(' ')[0]
+			filename = get_file_name(filename)
+			filename = get_basename(filename)
+			ehd[filename] = list(map(int, curLine.split(' ')[1:]))
+	return ehd
+
+
 def one_retrieval(my_args):
-	t_desc = my_args[0]
-	test_ind = my_args[1]
-	desc = my_args[2]
+	ehd = my_args[0]
+	test_name = my_args[1]
+	names = my_args[2]
 	top_n = my_args[3]
 	M_inv = my_args[4]
 
 	# cur_retrieval = sorted(range(len(desc)), key=lambda x: distance(t_desc[test_ind], desc[x], M))
 	cur_retrieval = []
-	for i in range(len(desc)):
-		dist = distance(t_desc[test_ind], desc[i], M_inv)
-		hq.heappush(cur_retrieval, (-1 * dist, i))
+	for name in names:
+		dist = distance(ehd[get_basename(test_name)], ehd[get_basename(name)], M_inv)
+		hq.heappush(cur_retrieval, (-1 * dist, name))
 		if len(cur_retrieval) > top_n:
 			hq.heappop(cur_retrieval)
 
-	print('Test img #', test_ind, 'done')
+	print('Test img #', test_name, 'done')
 	# return (test_ind, cur_retrieval[:top_n])
-	return (test_ind, [u[1] for u in cur_retrieval])
+	return (test_name, [u[1] for u in cur_retrieval])
 
 if __name__ == '__main__':
 	# name : 1000.png, result : ../IRMA/../../../1000.png
 	desc, labels, names = load_descriptor('train_dataset.txt_desc')
 	t_desc, t_labels,t_names = load_descriptor('test_dataset.txt_desc')
 
-	print('One desc len', len(desc[0]))
-
-	# chosen = [i for i in range(len(labels)) if int(labels[i]) in range(13, 17)]
-	chosen = list(range(len(desc)))
-	desc = [desc[i] for i in chosen]
-	labels = [labels[i] for i in chosen]
-	print('Using', len(desc), 'Images For training')
-
-
+	ehd = read_EHD()
+	X = [ ehd[get_basename(u)] for u in names ] 
+	X = np.array(X)
+	
 	lmnn = LMNN(max_iter=1)
 	print('Begin Metric Learning...')
-	lmnn.fit(np.array(desc), labels)
+	lmnn.fit(X, labels)
 	print('Finished Metric Learning')
 	M = lmnn.metric()
 	M_inv = linalg.inv(M)
@@ -71,14 +85,14 @@ if __name__ == '__main__':
 	top_n = 5
 	
 	with Pool() as pool:
-		ret_res = pool.map(one_retrieval, [ (t_desc, i, desc, top_n, M_inv) for i in range(len(t_desc)) ])
+		ret_res = pool.map(one_retrieval, [ (ehd, t_names[i], names, top_n, M_inv) for i in range(len(t_desc)) ])
 		for one_res in ret_res:
 			all_retrievals[one_res[0]] = one_res[1]
 
-	### evaluation using accuracy
+	# ### evaluation using accuracy
 	eval_res = []
 	for i in all_retrievals.keys():
-		correct = sum([ 1 for j in all_retrievals[i] if labels[j] == t_labels[i] ])
+		correct = sum([ 1 for j in all_retrievals[i] if labels[names.index(j)] == t_labels[t_names.index(i)] ])
 		eval_res += [correct / top_n]
 
 	print('[Mean Accuracy]', sum(eval_res) / len(eval_res))
